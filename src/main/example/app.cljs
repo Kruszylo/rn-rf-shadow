a3affa99ea9b4088f66da41190323c79a94e93d4
Unknown <m.titov@student.uj.edu.pl>
2019-09-16 16:21:35 +0200

    init simple navigation

--- src/main/example/app.cljs
+++ src/main/example/app.cljs
@@ -7,13 +7,8 @@
    [re-frame.core :as rf]
    [shadow.expo :as expo]
    [example.events]
-   [example.subs]))
-
-;; must use defonce and must refresh full app so metro can fill these in
-;; at live-reload time `require` does not exist and will cause errors
-;; must use path relative to :output-dir
-
-(defonce splash-img (js/require "../assets/shadow-cljs.png"))
+   [example.subs]
+   ["react-navigation-stack" :as ReactNavigationStack]))
 
 (def styles
   ^js (-> {:container
@@ -21,22 +16,6 @@
             :backgroundColor "#fff"
             :alignItems "center"
             :justifyContent "center"}
-           :title
-           {:fontWeight "bold"
-            :fontSize 24
-            :color "blue"}
-           :button
-           {:fontWeight "bold"
-            :fontSize 18
-            :padding 6
-            :backgroundColor "blue"
-            :borderRadius 10}
-           :buttonText
-           {:paddingLeft 12
-            :paddingRight 12
-            :fontWeight "bold"
-            :fontSize 18
-            :color "white"}
            :label
            {:fontWeight "normal"
             :fontSize 15
@@ -44,23 +23,21 @@
           (clj->js)
           (rn/StyleSheet.create)))
 
-(defn root []
-  (let [counter (rf/subscribe [:get-counter])]
-    (fn []
-      [:> rn/View {:style (.-container styles)}
-       [:> rn/Text {:style (.-title styles)} "Clicked: " @counter]
-       [:> rn/TouchableOpacity {:style (.-button styles)
-                                :on-press #(rf/dispatch [:inc-counter])}
-        [:> rn/Text {:style (.-buttonText styles)} "Click me, I'll count"]]
-       [:> rn/Image {:source splash-img :style {:width 200 :height 200}}]
-       [:> rn/Text {:style (.-label styles)} "Using: shadow-cljs+expo+reagent+re-frame"]])))
+(defn app []
+  [:> rn/View {:style (.-container styles)}
+   [:> rn/Text {:style (.-label styles)} "Home page"]])
 
-(defn start
-  {:dev/after-load true}
-  []
-  (expo/render-root (r/as-element [root])))
+(def routes {:Home {:screen (r/reactify-component app)}})
+
+(defonce nav-stack
+  (ReactNavigationStack/createStackNavigator
+   (clj->js routes)
+   #js {:initialRouteName "Home"}))
+
+(defn app-root []
+  [:> nav-stack {}])
 
 (defn init []
   (rf/dispatch-sync [:initialize-db])
-  (start))
+  (rn/AppRegistry.registerComponent "main" app-root))
 
